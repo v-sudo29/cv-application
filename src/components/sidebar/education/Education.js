6ebed7c128527f6f77a04a428b533a25cc718b3d
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-04-28 10:04:12 -0700

    Create skeleton for education content

--- src/components/sidebar/education/Education.js
+++ src/components/sidebar/education/Education.js
@@ -1,6 +1,14 @@
 export default function Education() {
   return (
-    <>
-    </>
+    <div className='education-div'>
+      <div className='education-title'>EDUCATION</div>
+      <div className='education-content'>
+        <div className='credential-div'>
+          <div className='credential-name'>B.A. New Media Design</div>
+          <div className='university-name'>Rochester Institute of Technology | New York</div>
+          <div className='university-years'>2014-2019</div>
+        </div>
+      </div>
+    </div>
   )
 }
